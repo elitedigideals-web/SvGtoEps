2289a7a6f0b4c35cc25b5eb983800baef1ba71eb
elitedigideals-web <elitedigideals@gmail.com>
2026-06-05 22:46:30 +0530

    Change app port from 5000 to 7860

--- app.py
+++ app.py
@@ -38,8 +38,7 @@ def convert():
     )
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=7860)
 
 @app.route('/manifest.json')
 def manifest():
